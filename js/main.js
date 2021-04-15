cf74681e4ab8605eddd3c51ee2837a5747033507
kirie0319 <matsueda0407@icloud.com>
2021-04-15 14:38:45 +0900

    出力領域を作ってそこを基準にした

--- js/main.js
+++ js/main.js
@@ -6,18 +6,15 @@
   btn.addEventListener('click', () => {
       const fizzNum = Number(document.getElementById('fizznum').value);
       const buzzNum = Number(document.getElementById('buzznum').value);
+      const output = document.getElementById('output');
+      output.innerHTML = '';
       if (!Number.isInteger(fizzNum) || !Number.isInteger(buzzNum) || fizzNum === 0 || buzzNum === 0) {
-        const e_Ul = document.createElement('ul');
-        const e_Li = document.createElement('li');
-        e_Li.textContent = '整数値を入力してください';
-        e_Ul.appendChild(e_Li);
-        document.body.appendChild(e_Ul);
+        output.textContent = '整数値を入力してください';
         return;
       }
-      const ul = document.createElement('ul');
     
       for (let i = 1; i < 100; i++) {
-        const li = document.createElement('li')
+        const li = document.createElement('p')
         if (i % fizzNum === 0 && i % buzzNum === 0) {
           li.textContent = 'FizzBuzz' + i;
         } else if (i % fizzNum === 0) {
@@ -28,13 +25,9 @@
           li.textContent = '';
         }
         
-        ul.appendChild(li);
+        output.appendChild(li);
       }
-      document.body.appendChild(ul);
-
-      if (ul.hasChildNodes()) {
-        ul.removeChild();
-      }
+      document.body.appendChild(output);
     
   });
   
